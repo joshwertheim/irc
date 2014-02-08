fe6745176bee92556e0da51fadd9bd017c395511
Joshua Wertheim <joshwertheim@gmail.com>
2014-02-08 11:08:01 -0800

    Initial commit. There's already a good amount of code.

---
+++ client.py
@@ -0,0 +1,94 @@
+import sys
+import socket
+import string
+import json
+
+class IRCClient(object):
+    """Creates an IRCClient instance with basic server config and functions"""
+
+    SERVER = ""
+    sock = ""
+    INIT_CHANNEL = "#spike"
+
+    def __init__(self):
+        self.sock = socket.socket()
+
+    def connect(self, server):
+        self.SERVER = server
+        self.sock.connect((self.SERVER, 6667))
+
+class User(object):
+    """Creates a User instance with basic user config and functions"""
+
+    NICK = ""
+    IDENT = ""
+    REALNAME = ""
+    PASS = ""
+
+    def __init__(self, client, nickname, identity, fullname, password):
+        self.NICK = nickname
+        self.IDENT = identity
+        self.REALNAME = fullname
+        self.PASS = password
+
+    def identify(self):
+        client.sock.send("NICK %s\r\n" % self.NICK)
+        client.sock.send("USER %s %s bla :%s\r\n" % (self.IDENT, client.SERVER, self.REALNAME))
+        client.sock.send("PASS %s\r\n" % self.PASS)
+        client.sock.send("JOIN %s\r\n" % client.INIT_CHANNEL)
+
+    def joinInitialChans(self):
+        client.sock.send("JOIN %s\r\n" % client.INIT_CHANNEL)
+
+
+print "Enter your nickname:"
+nickname = "spikebot" # raw_input()
+print "Enter your identity:"
+identity = "spikebot" # raw_input()
+print "Enter your real name:"
+fullname = "spikesbot" # raw_input()
+print "Enter your password:"
+password = raw_input() # some password 
+
+client = IRCClient()
+user = User(client, nickname, identity, fullname, password)
+client.connect("irc.hellcat.net")
+user.identify()
+
+active = 1
+readbuffer = ""
+
+quoteDatabase = {}
+
+while active:
+    readbuffer = readbuffer + client.sock.recv(1024)
+    temp = string.split(readbuffer, "\n")
+    readbuffer = temp.pop()
+
+    for line in temp:
+
+        if "Password accepted" in line:
+            print "joining"
+            user.joinInitialChans()
+            message = "Hello, world. :O"
+            client.sock.send("PRIVMSG #hellcat " + message + "\r\n")
+
+
+        line = string.rstrip(line)
+        line = string.split(line)
+
+
+        if ":@quote" in line:
+            quote = "\"" " ".join(line[5:len(line)]) + "\""
+            quoteDatabase[line[4]] = quote
+
+        if(line[0] == "PING"):
+            print(line)
+            client.sock.send("PONG %s\r\n" % line[1])
+            print("PONG %s\r\n" % line[1])
+
+        if ":@quit" in line:
+            active = 0
+
+with open('quoteDatabase.json', 'wb') as export:
+    json.dump(quoteDatabase, export)
